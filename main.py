bca2c788c29591a7978cf3764053a0638c667888
Mohamed Amine Frioua <aminefrioua@gmail.com>
2025-02-09 20:41:37 +0900

    run formatter and improve css

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-logging.basicConfig(level=logging.INFO)  
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
@@ -36,6 +36,7 @@ async def home():
         html_content = file.read()
     return HTMLResponse(content=html_content)
 
+
 @app.post("/chat")
 async def chat(request: Request):
     data = await request.json()
@@ -47,11 +48,15 @@ async def chat(request: Request):
         response = model.generate_content(
             f"You are a cute Japanese tutor. Teach Japanese in a fun and simple way. User says: {user_message}"
         )
-        logger.info(f"Google AI Studio API response: {response}")  # Log the entire response
+        logger.info(
+            f"Google AI Studio API response: {response}"
+        )  # Log the entire response
         bot_response = response.text
         logger.info(f"Bot response: {bot_response}")  # Log the extracted bot message
         return JSONResponse(content={"response": bot_response})
 
     except Exception as e:
         logger.exception(f"Error communicating with Google AI Studio: {e}")
-        return JSONResponse(status_code=500, content={"error": "Failed to generate a response"})
+        return JSONResponse(
+            status_code=500, content={"error": "Failed to generate a response"}
+        )
